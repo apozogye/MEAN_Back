30e1409fa33e82bb244ef3874b69190f93626904
apozo_gye <agpozo14@gmail.com>
2025-10-28 00:30:57 -0500

    Ultimos cambios antes de subir a aws

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app= require('./app')
-app.listen(app.get('puerto'), ()=>{
+app.listen(app.get('puerto'), '0.0.0.0', ()=>{
  console.log('Nombre de la App',app.get('nombreApp'));
  console.log('Puerto del servidor',app.get('puerto'));
 })
